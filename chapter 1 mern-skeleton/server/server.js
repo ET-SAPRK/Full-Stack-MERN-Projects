bfc2da0b30769932a3aedc2edca6175aa0fc980f
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-09-01 01:49:42 +0300

    edited

--- chapter 1 mern-skeleton/server/server.js
+++ chapter 1 mern-skeleton/server/server.js
@@ -1,8 +1,23 @@
 import "dotenv/config";
 import app from "./express.js";
-app.listen(process.env.port, (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.info("Server started on port %s.", process.env.port);
-});
+// app.listen(process.env.port, (err) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.info("Server started on port %s.", process.env.port);
+// });
+import mongoose from "mongoose";
+mongoose
+  .connect(process.env.mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(process.env.port, (err) => {
+      console.info("Server started on port %s.", process.env.port);
+    });
+  })
+  .catch((err) => {
+    console.log("Error Connecting to MongoDB");
+  });
