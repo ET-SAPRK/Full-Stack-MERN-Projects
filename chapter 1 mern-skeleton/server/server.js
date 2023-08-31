07b060ccb65801f4673f4dd71c3d3958ff15ac83
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-09-01 01:56:30 +0300

    setup success

--- chapter 1 mern-skeleton/server/server.js
+++ chapter 1 mern-skeleton/server/server.js
@@ -1,11 +1,6 @@
 import "dotenv/config";
 import app from "./express.js";
-// app.listen(process.env.port, (err) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.info("Server started on port %s.", process.env.port);
-// });
+
 import mongoose from "mongoose";
 mongoose
   .connect(process.env.mongoUri, {
