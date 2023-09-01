84266c5d6afede189ced3525ab2d71c6380ed2ce
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-09-01 13:46:04 +0300

    edited

--- chapter 1 mern-skeleton/server/routes/auth.routes.js
+++ chapter 1 mern-skeleton/server/routes/auth.routes.js
@@ -1,7 +1,13 @@
 import express from "express";
 import authCtrl from "../controllers/auth.controller.js";
+import authCtrl from "../controllers/auth.controller.js";
 
 const router = express.Router();
 router.route("/auth/signin").post(authCtrl.signin);
 router.route("/auth/signout").get(authCtrl.signout);
+router
+  .route("/api/users/:userId")
+  .get(authCtrl.requireSignin, userCtrl.read)
+  .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
+  .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove);
 export default router;
