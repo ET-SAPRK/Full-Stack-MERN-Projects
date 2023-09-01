dff503c43f819604578a3804120f255f2097e21e
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-09-01 12:51:17 +0300

    edited

--- chapter 1 mern-skeleton/server/routes/user.routes.js
+++ chapter 1 mern-skeleton/server/routes/user.routes.js
@@ -6,5 +6,7 @@ const router = express.Router();
 router.route("/api/users").post(userCtrl.create);
 router.route("/api/users").get(userCtrl.list);
 router.param("userId", userCtrl.userByID);
+router.route("/api/users/:userId").get(userCtrl.read);
+router.route("/api/users/:userId").put(userCtrl.update);
 
 export default router;
